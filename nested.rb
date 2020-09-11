b92dd013b3553ca5402dd36a8e28a98fa9a62234
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-11 23:07:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,14 +87,12 @@ def adding_matz
 					]
 				 }
      }
-<<<<<<< HEAD
 		 programmer_hash[:yukihiro_matsumoto] ={}
 		 programmer_hash[:yukihiro_matsumoto][:known_for]="Ruby"
 		 programmer_hash[:yukihiro_matsumoto][:languages]=["LISP", "C"]
 		 programmer_hash
-=======
 
->>>>>>> e91194abbdc3e96be8a82ce0440a35cf6f365187
+
 end
 
 def changing_alan
@@ -116,12 +114,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 
-<<<<<<< HEAD
+
 programmer_hash[:alan_kay][:known_for] = alans_new_info
 programmer_hash
-=======
-
->>>>>>> e91194abbdc3e96be8a82ce0440a35cf6f365187
 end
 
 def adding_to_dennis
